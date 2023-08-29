ae18ea713f2114bc8503e1742d235e7c0f95c8e4
Favour Adekogbe <favouradekogbe@gmail.com>
2023-08-29 13:22:36 +0100

    UI fixes

--- src/features/authentication/resetPassword/index.tsx
+++ src/features/authentication/resetPassword/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import styles from "./styles.module.css";
+import styles from "./styles.module.scss";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm, SubmitHandler } from "react-hook-form";
 import * as yup from "yup";
