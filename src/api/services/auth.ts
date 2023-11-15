076d24eeff18ff8599a4c90ca4d6f12be19050a7
Favour Adekogbe <favouradekogbe@gmail.com>
2023-11-16 00:01:26 +0100

    reset

--- src/api/services/auth.ts
+++ src/api/services/auth.ts
@@ -4,61 +4,9 @@ AUTH SERVICES
 =================================
 */
 
-import {
-  axiosInstanceUnauth,
-  loginURL,
-  newPasswordURL,
-  refreshTokenURL,
-  resetPasswordURL,
-} from "api";
-
-interface loginData {
-  email: string;
-  password: string;
-}
 /**
  * login service
  * @returns axios promise
  */
 
-export const loginService = (data: loginData) => {
-  return axiosInstanceUnauth.post(loginURL(), data);
-};
-
-interface resetPasswordData {
-  email: string;
-}
-/**
- * request password reset service
- * @returns axios promise
- */
-
-export const resetPasswordService = (data: resetPasswordData) => {
-  return axiosInstanceUnauth.post(resetPasswordURL(), data);
-};
-
-interface newPasswordData {
-  password: string;
-  uid64: string;
-  token: string;
-}
-/**
- * reset password service
- * @returns axios promise
- */
-
-export const newPasswordService = (data: newPasswordData) => {
-  return axiosInstanceUnauth.patch(newPasswordURL(), data);
-};
-
-interface refreshTokenData {
-  refresh: string;
-}
-/**
- * refresh JWT service
- * @returns axios promise
- */
-
-export const refreshTokenService = (data: refreshTokenData) => {
-  return axiosInstanceUnauth.post(refreshTokenURL(), data);
-};
+export const loginService = () => {};
--- src/api/urls/auth.ts
+++ src/api/urls/auth.ts
@@ -11,31 +11,4 @@ AUTH URLS
  *
  */
 
-export const loginURL = () => `/auth/login/`;
-
-/**
- * Request Password Reset URL
- *
- * @returns url string
- *
- */
-
-export const resetPasswordURL = () => `/auth/request-reset-password-email/`;
-
-/**
- * Password Reset URL
- *
- * @returns url string
- *
- */
-
-export const newPasswordURL = () => `/auth/set-new-password/`;
-
-/**
- * Refresh JWT URL
- *
- * @returns url string
- *
- */
-
-export const refreshTokenURL = () => `/auth/refresh-token/`;
+export const loginURL = () => ``;
